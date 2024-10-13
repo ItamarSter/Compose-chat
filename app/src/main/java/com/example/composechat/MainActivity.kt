71e006989a46aae8d6723a64b57e435c898ba86f
ItamarSter <93194252+ItamarSter@users.noreply.github.com>
2024-10-13 12:00:27 +0300

    initial commit

---
+++ app/src/main/java/com/example/composechat/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.composechat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composechat.ui.theme.ComposeChatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeChatTheme {
+
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeChatTheme {
+
+    }
+}
---
+++ app/src/main/java/com/example/composechat/ui/screens/ChatScreen.kt
@@ -0,0 +1,8 @@
+package com.example.composechat.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatScreen() {
+
+}
