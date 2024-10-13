de5e8600dd3c6cc370ffc048602fb96027d21e15
ItamarSter <93194252+ItamarSter@users.noreply.github.com>
2024-10-13 22:53:49 +0300

    design side drawer

--- app/src/main/java/com/example/composechat/ui/components/SideDrawer.kt
+++ app/src/main/java/com/example/composechat/ui/components/SideDrawer.kt
@@ -1,20 +1,24 @@
 package com.example.composechat.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import com.example.composechat.R
 import kotlinx.coroutines.launch
 
 @Composable
@@ -26,12 +30,27 @@ fun SideDrawer(
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet {
-                Text(text = "title")
-                Text(text = "sec title")
+            ModalDrawerSheet(
+                modifier = Modifier.width(300.dp)
+            ) {
+                NavigationDrawerItem(
+                    label = {
+                        Text(text = stringResource(id = R.string.create_new_chat))
+                    },
+                    shape = RectangleShape,
+                    selected = false,
+                    onClick = { }
+                )
+                NavigationDrawerItem(
+                    label = {
+                        Text(text = stringResource(id = R.string.join_to_chat))
+                    },
+                    shape = RectangleShape,
+                    selected = false,
+                    onClick = { }
+                )
             }
-        },
-        gesturesEnabled = true
+        }
     ) {
         Column {
             IconButton(onClick = {
